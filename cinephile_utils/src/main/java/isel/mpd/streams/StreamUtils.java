9db7c10345764ec5214bb5fed73eed5e89ce6c79
Eduardo Conceição <eduardo.301101@gmail.com>
2023-06-04 01:48:48 +0100

    implemented generalized sortedInterception method

--- cinephile_utils/src/main/java/isel/mpd/streams/StreamUtils.java
+++ cinephile_utils/src/main/java/isel/mpd/streams/StreamUtils.java
@@ -11,4 +11,14 @@ public class StreamUtils {
 	public static <T> Stream<T> sortedIntersection(Comparator<T> cmp, Stream<T> s1, Stream<T> s2) {
 		return StreamSupport.stream(new SortedIntersection<>(cmp, s1, s2), false);
 	}
+
+	@SafeVarargs
+	public static <T> Stream<T> sortedIntersection(Comparator<T> cmp, Stream<T>... streams) {
+		Stream<T> res = streams[0];
+		for (int i = 1; i < streams.length; ++i) {
+			res = res.toList().stream(); // Regenerate 'res'
+			res = sortedIntersection(cmp, res, streams[i]);
+		}
+		return res;
+	}
 }
--- cinephile_utils/src/test/java/isel/leirt/mpd/moviesdb2/tests/StreamUtilsTests.java
+++ cinephile_utils/src/test/java/isel/leirt/mpd/moviesdb2/tests/StreamUtilsTests.java
@@ -7,15 +7,10 @@ import static org.junit.Assert.*;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import static java.util.stream.Stream.*;
-
 
 public class StreamUtilsTests {
-	
 	@Test
 	public void intersectionTest() {
 		Comparator<Integer> cmp = Comparator.comparing((Integer v) -> v);
@@ -25,6 +20,24 @@ public class StreamUtilsTests {
 		var expected = List.of(3, 5, 7);
 
 		Stream<Integer> res = sortedIntersection(cmp, ints1, ints2);
+		var list = res.collect(Collectors.toList());
+		System.out.println(list);
+
+		assertEquals(expected, list);
+	}
+
+	@Test
+	public void intersectionTest2() {
+		Comparator<Integer> cmp = Comparator.comparing((Integer v) -> v);
+
+		Stream<Integer> ints1 = Stream.of(3, 1, 5, 9, 7, 7).sorted(cmp);
+		Stream<Integer> ints2 = Stream.of(5, 3, 4, 7, 10, 12).sorted(cmp);
+		Stream<Integer> ints3 = Stream.of(5, 3, 7).sorted(cmp);
+		Stream<Integer> ints4 = Stream.of(11, 9, 3, 7, 21, 5).sorted(cmp);
+		Stream<Integer> ints5 = Stream.of(55, 55, 7, 5, 88, 5, 2, 3, 200).sorted(cmp);
+		var expected = List.of(3, 5, 7);
+
+		Stream<Integer> res = sortedIntersection(cmp, ints1, ints2, ints3, ints4, ints5);
 		var list = res.collect(Collectors.toList());
 		System.out.println(list);
 
