3a1d92f7edd321c964fc91d3324e793e8fd91e09
Eduardo Conceição <eduardo.301101@gmail.com>
2023-06-19 01:32:09 +0100

    implemented ex1 and ex2

--- trab2-fase3/src/main/java/more_streams/StreamUtils.java
+++ trab2-fase3/src/main/java/more_streams/StreamUtils.java
@@ -1,5 +1,8 @@
 package more_streams;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
@@ -11,5 +14,44 @@ public class StreamUtils {
             }
             return Stream.of();
         });
+    }
+
+    // converts all characters from the strings between 'start' and 'max' in a list to lower case.
+    // string length must be greater than 10
+    public static List<String> ex1Original(List<String> lines, int start, int max) {
+        int i = start;
+        List<String> result = new ArrayList<>();
+        while(i - start <= max && i < lines.size() && lines.get(i).length() > 10) {
+            result.add(lines.get(i++).toLowerCase());
+        }
+        return result;
+    }
+
+    public static List<String> ex1(List<String> lines, int start, int max) {
+        return lines.stream()
+                .skip(start)
+                .limit(max + 1)
+                .filter(line -> line.length() > 10)
+                .map(String::toLowerCase)
+                .toList();
+    }
+
+    // removes duplicate strings from a nested list and returns a sorted list of unique strings
+    public static List<String> ex2Original(List<List<String>> text) {
+        var result = new ArrayList<String>();
+        for(var l : text) {
+            for(var s : l) {
+                if (!result.contains(s)) result.add(s);
+            }
+        }
+        result.sort(Comparator.naturalOrder());
+        return result;
+    }
+
+    public static List<String> ex2(List<List<String>> text) {
+        return text.stream()
+                .flatMap(List::stream)
+                .distinct()
+                .toList();
     }
 }
--- trab2-fase3/src/test/java/more_streams/StreamUtilTests.java
+++ trab2-fase3/src/test/java/more_streams/StreamUtilTests.java
@@ -3,6 +3,7 @@ package more_streams;
 import org.junit.jupiter.api.Test;
 import sequences.Sequence;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -22,5 +23,46 @@ public class StreamUtilTests {
         var res2 = StreamUtils.filter(ex2, n -> n.length() > 3 );
         var expected2 = List.of("Tele");
         assertEquals(res2.toList(), expected2);
+    }
+
+    @Test
+    public void ex1Test() {
+        List<String> l = new ArrayList<>();
+        l.add("This is LINE 1");
+        l.add("This is LINE 2");
+        l.add("This is LINE 3");
+        l.add("This is LINE 4");
+        l.add("This is LINE 5");
+
+        var expected = StreamUtils.ex1Original(l, 1, 3);
+        var res = StreamUtils.ex1(l, 1, 3);
+
+        assertEquals(expected, res);
+    }
+
+    @Test
+    public void ex2Test() {
+        List<String> l1 = new ArrayList<>();
+        l1.add("This is line 1");
+        l1.add("This is line 2");
+        l1.add("This is line 3");
+        l1.add("This is line 4");
+        l1.add("This is line 5");
+
+        List<String> l2 = new ArrayList<>();
+        l2.add("This is line 3"); // duplicate
+        l2.add("This is line 4"); // duplicate
+        l2.add("This is line 5"); // duplicate
+        l2.add("This is line 6");
+        l2.add("This is line 7");
+
+        List<List<String>> listList = new ArrayList<>();
+        listList.add(l1);
+        listList.add(l2);
+
+        var expected = StreamUtils.ex2Original(listList);
+        var res = StreamUtils.ex2(listList);
+
+        assertEquals(expected, res);
     }
 }
